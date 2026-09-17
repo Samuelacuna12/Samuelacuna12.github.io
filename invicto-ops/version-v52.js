bfaa9e4e90f9c1b8eabd48e2df12a1c95e8ac124
Samuelacuna12 <samuelatuna@gmail.com>
2026-09-16 22:36:08 -0500

    INVICTO OPS v52.1: actualizar versión

--- invicto-ops/version-v52.js
+++ invicto-ops/version-v52.js
@@ -1,5 +1,5 @@
-/* INVICTO OPS v52.0 */
+/* INVICTO OPS v52.1 */
 (function(){
-  function apply(){const f=document.querySelector('.sidebar-foot');if(f)f.innerHTML=f.innerHTML.replace(/Versión\s+[^<]+/,'Versión 52.0');}
+  function apply(){const f=document.querySelector('.sidebar-foot');if(f)f.innerHTML=f.innerHTML.replace(/Versión\s+[^<]+/,'Versión 52.1');}
   apply();setTimeout(apply,300);setTimeout(apply,1500);
 })();
